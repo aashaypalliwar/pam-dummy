36be88e06b4d38bf72a6260deb23320081c90595
Aashay Palliwar <45427968+aashaypalliwar@users.noreply.github.com>
2022-04-23 18:01:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/:clientEndpoint', (req, res, next) => {
-    console.log("PAM says: " + req.params.clientEndpoint + "!");
-    res.send("pam says: " + req.params.clientEndpoint);
+    console.log("!!PAM says: " + req.params.clientEndpoint + "!");
+    res.send("!!pam says: " + req.params.clientEndpoint);
 })
 
 module.exports = app;
